72488ad707afda5f433e674cfea889922816aac2
DavidMilubaya <King663663@qq.com>
2026-07-29 09:55:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // --- 场景 ---
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x88ccff);
+scene.background = new THREE.Color(0xCD5C5C );
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 1.7, 5);
